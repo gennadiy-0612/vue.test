769247c345fa73fb85206a2df8e33ceecc681364
Genn <intval@i.ua>
2021-06-02 14:26:46 +0300

    start

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,3 @@
-<script>
-// Home.vue
-export default {
-  computed: {
-    username() {
-      // We will see what `params` is shortly
-      return this.$route.params.username
-    }
-  },
-  methods: {
-    goBack() {
-      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
-    }
-  }
-}
-</script>
+<template>
+  <h1>kkk</h1>
+</template>

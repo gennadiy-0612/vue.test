f3d3fceec40c8290f5ebf70ace7c146fa600c259
Genn <intval@i.ua>
2021-06-03 10:32:10 +0300

    start

--- src/components/litboxgreen/Index.vue
+++ src/components/litboxgreen/Index.vue
@@ -1,15 +1,43 @@
 <template>
-  <div id="info" class="lightbox">
+  <div id="info" class="LightBoxGreen">
     <a href="#" class="closePopUp block absolute right-0"></a>
-    <p>Пепперони</p>
-    <p>Пищевая ценность на 100 г</p>
-    <div>
-      <table>
-        <tr>
-          <td>Энерг. ценность</td>
-          <td>253,4 ккал</td>
-        </tr>
+    <p class="Ag-h3-to-1070 text-white">Пепперони</p>
+    <p class="Ag-text12-to-1070 color7E7E7E p-tb5">Пищевая ценность на 100 г</p>
+      <table class="w-full">
+        <tr><td class="text-white Ag-text14-to-1070 line-h">Энерг. ценность</td><td class="text-white text-right">253,4 ккал</td></tr>
+        <tr><td class="text-white Ag-text14-to-1070 line-h">Белки</td><td class="text-white text-right">9,4 г</td></tr>
+        <tr><td class="text-white Ag-text14-to-1070 line-h">Жиры</td><td class="text-white text-right">7,1 г</td></tr>
+        <tr><td class="text-white Ag-text14-to-1070 line-h">Углеводы</td><td class="text-white text-right">36,3 г</td></tr>
+        <tr><td class="text-white Ag-text14-to-1070 line-h">Вес</td><td class="text-white text-right">700 г</td></tr>
       </table>
-    </div>
   </div>
 </template>
+
+<style>
+
+/* Закрываем lightbox */
+.LightBoxGreen {
+  display: none;
+  padding: 16px 16px 10px;
+}
+
+.closePopUp {
+  top: -26px;
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  background: url("../../assets/img/infoGreen.svg") center center no-repeat;
+}
+
+/* Открываем lightbox */
+.LightBoxGreen:target {
+  position: absolute;
+  left: 0;
+  top: 26px;
+  width: 100%;
+  display: block;
+  align-items: center;
+  justify-content: center;
+  background-color: #465500;
+}
+</style>
--- src/components/popupbluda/DescriptPrice.vue
+++ src/components/popupbluda/DescriptPrice.vue
@@ -1,20 +1,8 @@
 <template>
   <div class="Description relative">
     <a class="showPopUp block absolute right-0" href="#info"></a>
-    <div id="info" class="lightbox">
-      <a href="#" class="closePopUp block absolute right-0"></a>
-      <p>Пепперони</p>
-      <p>Пищевая ценность на 100 г</p>
-      <div>
-        <table>
-          <tr>
-            <td>Энерг. ценность</td>
-            <td>253,4 ккал</td>
-          </tr>
-        </table>
-      </div>
-    </div>
-    <h3 class="Ag-h3-to-1070 m-top14">{{ msg }}</h3>
+    <LiteBoxGreen/>
+    <h3 class="Ag-h3-to-1070 m-top14">Пепперони</h3>
     <p class="Ag-text12-to-1070 m-top11">Средняя 30 см, традиционное тесто, 700 г</p>
     <p class="Ag-text14-to-1070 m-top16 m-r16">Пикантная колбаса Пепперони, свежие томаты, маслины, каперсы, сыр
       Моцарелла и соус Помадоро</p>
@@ -22,11 +10,21 @@
   </div>
 </template>
 
+<style>
+.showPopUp {
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  background: url("../../assets/img/info.svg") center center no-repeat;
+}
+</style>
 <script>
+import LiteBoxGreen from '../litboxgreen/Index.vue'
+
 export default {
-  name: 'Description',
-  props: {
-    msg: String
+  name: 'App',
+  components: {
+    LiteBoxGreen
   }
 }
 </script>

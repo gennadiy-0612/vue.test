cd0045c16fb91f7b6c0e3f2bfccf77b35bd193c0
Genn <intval@i.ua>
2021-06-08 12:43:04 +0300

    page-with-contacts-complete

--- src/components/burgmenu/MenuContent.vue
+++ src/components/burgmenu/MenuContent.vue
@@ -3,7 +3,7 @@
     <a href="#" class="closeX fixed right-2 z-2 block top-0">
       <X/>
     </a>
-    <ol class="flex flex-col justify-center text-center absolute bg-white left-0 top-0 w-full h-full z-1 E4E4E4 overflow-y-scroll">
+    <ol class="OlContent flex flex-col justify-center text-center absolute bg-white left-0 top-0 w-full h-full z-1 E4E4E4 overflow-y-scroll">
       <li class="MenuContent__li"><a class="Ag6001417" href="/card">Шаблон</a></li>
       <li class="MenuContent__li"><a class="Ag6001417" href="/popupbluda">popupbluda</a></li>
       <li class="MenuContent__li"><a class="Ag6001417" href="/popupcombo">popupcombo</a></li>
@@ -43,6 +43,9 @@
   color: #009200;
   transition-property: color;
   transition-duration: .7s;
+}
+.OlContent{
+  padding: 90px 0 0;
 }
 </style>
 <script>

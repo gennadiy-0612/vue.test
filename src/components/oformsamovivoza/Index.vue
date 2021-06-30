00b832026b0753d10ab93ae25c1f31f2b87c6948
Genn <intval@i.ua>
2021-06-30 19:38:59 +0300

    page-with-contacts-complete

--- src/components/oformsamovivoza/Index.vue
+++ src/components/oformsamovivoza/Index.vue
@@ -4,8 +4,7 @@
       <Address/>
       <BurgMenu/>
     </div>
-    <div
-        class="OrderMoveAddresses xl:h-auto float-left bg-white xl:items-end xl:flex-col CenteringOneItem xl:h-full flex flex-col justify-center items-center xl:items-start">
+    <div class="OrderMoveAddresses float-left bg-white xl:items-end xl:flex-col CenteringOneItem xl:h-full flex flex-col justify-center items-center xl:items-start">
       <AddressTime/>
     </div>
     <div class="DescHide">

6c8ca9256bcab4a97a82fde67256ac02deb06fcc
Genn <intval@i.ua>
2021-06-30 14:32:01 +0300

    page-with-contacts-complete

--- src/components/descimg/DescItem.vue
+++ src/components/descimg/DescItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="desc"><router-link to="/" class="mainItem text-center Ag-text14-to-1070 Ag700 relative block">{{ msg }}</router-link></div>
+  <div class="desc xl:flex xl:h-full xl:w-full xl:align-center"><router-link to="/" class="mainItem text-center Ag-text14-to-1070 Ag700 relative block">{{ msg }}</router-link></div>
 </template>
 <style>
 .desc {
